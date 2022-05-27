4378215e1bd19c60dbdbffed2aa1b4db77a08231
Sébastien Bühl <buhlsebastien@gmail.com>
2022-05-27 19:17:12 +0200

    [FIRST] inital commit

---
+++ __manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "web",
+
+    'summary': """
+        Static files for Web""",
+
+    'description': """
+        
+    """,
+
+    'author': "Sébastien Bühl",
+    'website': "http://www.buhl.be",
+
+    'category': 'Customizations',
+    'version': '0.0.0.1',
+    'license': 'LGPL-3',
+
+    # any module necessary for this one to work correctly
+    'depends': [],
+
+    'data': [
+
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'web/static/pdf/presentation.pdf',
+        ]
+    },
+}
